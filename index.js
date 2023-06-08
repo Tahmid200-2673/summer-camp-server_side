3629d53622a76b4b0cd5cc33f1cc78b84177b830
Tahmid200-2673 <tahmidahmed22673@gmail.com>
2023-06-09 00:02:02 +0600

    users data fetching

--- index.js
+++ index.js
@@ -10,6 +10,58 @@ app.use(express.json());
 
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yrxlyvn.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+     await client.connect();
+  
+
+
+    
+    const usersCollection = client.db("sports").collection("users");
+
+
+
+    //user realated apis
+    app.get('/users', async (req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      });
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+       console.log(user);
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+      
+  
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('sports is running')
 })
