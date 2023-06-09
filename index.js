d00401276e1f74c885f42f2514b82671cdd2e669
Tahmid200-2673 <tahmidahmed22673@gmail.com>
2023-06-10 02:32:28 +0600

    class server and user added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yrxlyvn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,7 +31,8 @@ async function run() {
 
 
     
-    const usersCollection = client.db("sports").collection("users");
+     const usersCollection = client.db("sports").collection("users");
+     const classesCollection = client.db("sports").collection("classes");
 
 
 
@@ -44,9 +45,134 @@ async function run() {
     app.post('/users', async (req, res) => {
         const user = req.body;
        console.log(user);
+       const query = { email: user.email }
+       const existingUser = await usersCollection.findOne(query);
+
+       if (existingUser) {
+         return res.send({ message: 'user already exists' })
+       }
         const result = await usersCollection.insertOne(user);
         res.send(result);
       });
+
+      // check admin
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+
+      // if (req.decoded.email !== email) {
+      //   res.send({ admin: false })
+      // }
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'Admin' }
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'Admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+      // check instructor
+    app.get('/users/instructor/:email', async (req, res) => {
+      const email = req.params.email;
+
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === 'Instructor' }
+      res.send(result);
+    })
+
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'Instructor'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+    
+    app.get('/users/instructors', async (req, res) => {
+      try {
+        const instructors = await usersCollection.find({ role: 'Instructor' }).toArray();
+        res.send(instructors);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+    
+
+    
+
+    
+
+     //classes related apis
+
+     app.get('/classes', async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+     app.post('/classes', async (req, res) => {
+      const classes = req.body;
+      console.log(classes)
+      const result = await classesCollection.insertOne(classes);
+      res.send(result);
+  });
+
+  app.put('/classes/:id', async (req, res) => {
+    try {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedClass = req.body;
+  
+      const updateFields = {
+        $set: {
+          status: updatedClass.status,
+          feedback: updatedClass?.feedback,
+        },
+      };
+  
+      const result = await classesCollection.updateOne(filter, updateFields);
+      res.send(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+  app.get('/classes/approved', async (req, res) => {
+    try {
+      // Fetch the approved classes from the database
+      const approvedClasses = await classesCollection.find({ status: 'approved' }).toArray();
+      res.send(approvedClasses);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
+
+
 
       
   
